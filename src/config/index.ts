4aacb98297feef33ada31a0adb21d12ebc15eb6d
ddzaytsev <DDZaytsev@sberbank.ru>
2022-04-13 17:12:29 +0300

    init

---
+++ src/config/index.ts
@@ -0,0 +1,24 @@
+import { ChainId } from '@pancakeswap/sdk'
+import BigNumber from 'bignumber.js/bignumber'
+import { BIG_TEN } from 'utils/bigNumber'
+
+BigNumber.config({
+  EXPONENTIAL_AT: 1000,
+  DECIMAL_PLACES: 80,
+})
+
+export const BSC_BLOCK_TIME = 3
+
+export const BASE_BSC_SCAN_URLS = {
+  [ChainId.MAINNET]: 'https://bscscan.com',
+  [ChainId.TESTNET]: 'https://testnet.bscscan.com',
+}
+
+export const CAKE_PER_BLOCK = 40
+export const BLOCKS_PER_YEAR = (60 / BSC_BLOCK_TIME) * 60 * 24 * 365 // 10512000
+export const CAKE_PER_YEAR = CAKE_PER_BLOCK * BLOCKS_PER_YEAR
+export const BASE_URL = 'https://pancakeswap.finance'
+export const DEFAULT_TOKEN_DECIMAL = BIG_TEN.pow(18)
+export const DEFAULT_GAS_LIMIT = 250000
+export const IPFS_GATEWAY = 'https://ipfs.io/ipfs'
+
